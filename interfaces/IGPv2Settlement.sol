d162b840836c43b5d1a812143343845499c6b0a7
charlesndalton <charlesndalton@gmail.com>
2022-07-31 14:27:35 -0700

    refactor: change CowAnywhere -> Milkman, with added pairing logic

--- interfaces/IGPv2Settlement.sol
+++ interfaces/IGPv2Settlement.sol
@@ -6,4 +6,9 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IGPv2Settlement {
     function setPreSignature(bytes calldata orderUid, bool signed) external;
+
+    function filledAmount(bytes calldata orderUid)
+        external
+        view
+        returns (uint256);
 }
