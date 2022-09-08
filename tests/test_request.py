1b032f6989a742885e6b33dde66d6ab9f429ec09
charlesndalton <charlesndalton@gmail.com>
2022-09-07 17:30:42 -0700

    test: update request tests

--- tests/test_request.py
+++ tests/test_request.py
@@ -1,90 +1,124 @@
 from brownie import ZERO_ADDRESS, reverts
-from eth_abi import encode_abi
-from eth_utils import keccak
+import utils
 
 
-def test_request_swap(milkman, user, gno_whale, gno, dai, univ2_price_checker):
-    amount = 100e18
-    gno.transfer(user, amount, {"from": gno_whale})
-
-    gno.approve(milkman, amount, {"from": user})
+def test_request_swap(
+    milkman,
+    user,
+    token_to_sell,
+    token_to_buy,
+    amount,
+    price_checker,
+    price_checker_data,
+):
+    token_to_sell.approve(milkman, amount, {"from": user})
 
     tx = milkman.requestSwapExactTokensForTokens(
-        int(amount), gno, dai, user, univ2_price_checker, {"from": user}
+        int(amount),
+        token_to_sell,
+        token_to_buy,
+        user,
+        price_checker,
+        price_checker_data,
+        {"from": user},
     )
 
     assert tx.events.count("SwapRequested") == 1
 
     assert milkman.nonce() == 1
 
-    check_swap_requested(
-        milkman, user, user, gno, dai, amount, univ2_price_checker.address, 0
+    utils.check_swap_requested(
+        milkman,
+        user,
+        user,
+        token_to_sell,
+        token_to_buy,
+        amount,
+        price_checker.address,
+        price_checker_data,
+        0,
     )
 
 
-def test_request_swap_twice(milkman, user, gno_whale, gno, dai):
-    amount = 100e18
-    gno.transfer(user, amount, {"from": gno_whale})
+def test_request_swap_twice(
+    milkman,
+    user,
+    token_to_sell,
+    token_to_buy,
+    amount,
+    price_checker,
+    price_checker_data,
+):
 
-    gno.approve(milkman, amount, {"from": user})
+    token_to_sell.approve(milkman, amount, {"from": user})
 
     tx = milkman.requestSwapExactTokensForTokens(
-        int(amount / 2), gno, dai, user, ZERO_ADDRESS, {"from": user}
+        int(amount / 2),
+        token_to_sell,
+        token_to_buy,
+        user,
+        price_checker,
+        price_checker_data,
+        {"from": user},
     )
 
     assert tx.events.count("SwapRequested") == 1
 
     tx = milkman.requestSwapExactTokensForTokens(
-        int(amount / 2), gno, dai, user, ZERO_ADDRESS, {"from": user}
+        int(amount / 2),
+        token_to_sell,
+        token_to_buy,
+        user,
+        price_checker,
+        price_checker_data,
+        {"from": user},
     )
 
     assert tx.events.count("SwapRequested") == 1
 
     assert milkman.nonce() == 2
+    
 
-    check_swap_requested(milkman, user, user, gno, dai, amount / 2, ZERO_ADDRESS, 0)
-    check_swap_requested(milkman, user, user, gno, dai, amount / 2, ZERO_ADDRESS, 1)
-
-
-def test_request_without_sufficient_tokens(
-    milkman, user, gno, dai, univ2_price_checker
-):
-    amount = 100e18
-
-    with reverts():
-        milkman.requestSwapExactTokensForTokens(
-            int(amount), gno, dai, user, univ2_price_checker, {"from": user}
-        )
-
-    gno.approve(milkman, amount, {"from": user})
-
-    with reverts():
-        milkman.requestSwapExactTokensForTokens(
-            int(amount), gno, dai, user, univ2_price_checker, {"from": user}
-        )
-
-
-def check_swap_requested(
-    milkman, user, receiver, from_token, to_token, amount, price_checker_address, nonce
-):
-    # user, receiver, from_token, to_token, amount_in, price_checker, nonce
-    encoded_market_order = encode_abi(
-        ["address", "address", "address", "address", "uint256", "address", "uint256"],
-        [
-            user.address,
-            receiver.address,
-            from_token.address,
-            to_token.address,
-            int(amount),
-            price_checker_address,
-            nonce,
-        ],
+    utils.check_swap_requested(
+        milkman,
+        user,
+        user,
+        token_to_sell,
+        token_to_buy,
+        amount / 2,
+        price_checker.address,
+        price_checker_data,
+        0,
     )
-    swap_id = keccak(encoded_market_order)
-    swap_data = milkman.swaps(swap_id)
+    utils.check_swap_requested(
+        milkman,
+        user,
+        user,
+        token_to_sell,
+        token_to_buy,
+        amount / 2,
+        price_checker.address,
+        price_checker_data,
+        1,
+    )
 
-    swap_requested_data = encode_abi(["uint256"], [int(1)])
 
-    print(f"Swap Data: {swap_data}")
-    print(f"Swap Requested Data: {swap_requested_data}")
-    assert swap_data.hex() == swap_requested_data.hex()
+def test_revert_without_token_move(
+    milkman,
+    user,
+    token_to_sell,
+    token_to_buy,
+    amount,
+    price_checker,
+    price_checker_data,
+):
+    with reverts():
+        milkman.requestSwapExactTokensForTokens(
+            int(amount / 2),
+            token_to_sell,
+            token_to_buy,
+            user,
+            price_checker,
+            price_checker_data,
+            {"from": user},
+        )
